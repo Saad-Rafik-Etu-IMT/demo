32433d4eab850dcf969e937fb6602cf1ba972954
frankl1nzhu <zhuyzmalo@gmail.com>
2026-01-08 11:18:35 +0100

    test12

--- src/test/java/com/bfb/BfbManagementApplicationTests.java
+++ src/test/java/com/bfb/BfbManagementApplicationTests.java
@@ -3,7 +3,7 @@ package com.bfb;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-//testasd
+//testa
 @SpringBootTest
 @Disabled("Integration test - requires PostgreSQL connection")
 class BfbManagementApplicationTests {
